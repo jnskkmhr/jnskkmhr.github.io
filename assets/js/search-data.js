7b083de923bda102064fcc4172c77bca1416809e
jnskkmhr <70871522+jnskkmhr@users.noreply.github.com>
2026-01-31 20:57:50 +0000

    Deploying to gh-pages from @ jnskkmhr/jnskkmhr.github.io@6714ea2d24399e6446db37c9788f71210b4b2381 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -17,7 +17,7 @@ ninja.data = [{
             window.location.href = "/publications/";
           },
         },{id: "nav-projects",
-          title: "projects",
+          title: "Projects",
           description: "Here are my research projects.",
           section: "Navigation",
           handler: () => {
